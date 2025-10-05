6471a143034390ead54be8e7d3259db0baeb580a
Fabian Poels <fabian@fabianpoels.com>
2025-10-06 00:35:43 +0200

    cache ascents

--- controllers/public.controller.go
+++ controllers/public.controller.go
@@ -1,8 +1,10 @@
 package controllers
 
 import (
+	"encoding/json"
 	"net/http"
 
+	"github.com/fabianpoels/fabianpoels-api-go/cache"
 	"github.com/fabianpoels/fabianpoels-api-go/collections"
 	"github.com/fabianpoels/fabianpoels-api-go/db"
 	"github.com/fabianpoels/fabianpoels-api-go/models"
@@ -13,7 +15,17 @@ import (
 
 type PublicController struct{}
 
+const cacheKey = "ascents:public"
+
 func (ctrl PublicController) Ascents(c *gin.Context) {
+	// try to load from cache
+	cacheAscents, err := publicAscentsFromCache(c)
+	if err == nil {
+		c.JSON(http.StatusOK, cacheAscents)
+		return
+	}
+
+	// load from DB
 	mongoClient := db.GetDbClient()
 
 	cursor, err := collections.GetAscentCollection(mongoClient).Find(c, bson.M{}, options.Find().SetSort(bson.M{"number": -1}))
@@ -34,5 +46,28 @@ func (ctrl PublicController) Ascents(c *gin.Context) {
 		publicAscents[i] = models.SerializeAscent(ascent)
 	}
 
+	storePublicAscentsInCache(c, publicAscents)
+
 	c.JSON(http.StatusOK, publicAscents)
+}
+
+func publicAscentsFromCache(c *gin.Context) (ascents []models.PublicAscent, err error) {
+	cacheService := cache.Service{C: c}
+	cacheString, err := cacheService.Get(cacheKey)
+	if err != nil {
+		return ascents, err
+	}
+
+	err = json.Unmarshal([]byte(cacheString), &ascents)
+	if err != nil {
+		return ascents, err
+	}
+
+	return ascents, nil
+}
+
+func storePublicAscentsInCache(c *gin.Context, ascents []models.PublicAscent) {
+	cacheService := cache.Service{C: c}
+	cacheString, _ := json.Marshal(ascents)
+	cacheService.Set(cacheKey, cacheString, 0)
 }
